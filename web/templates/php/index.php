92e376a6d837dbb5f993469ada6bedaf7a738bda
el-gringo-alto <22480930+el-gringo-alto@users.noreply.github.com>
2022-01-14 00:52:08 -0500

    remove php files

--- web/templates/php/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include $_SERVER['DOCUMENT_ROOT'] . '/../templates/header.php';
-    include $_SERVER['DOCUMENT_ROOT'] . '/../templates/select-party.php';
-    include $_SERVER['DOCUMENT_ROOT'] . '/../templates/noscript.php';
-?>
-
-<section id="stream" class="tweet-feed" aria-label="Tweet feed"></section>
-
-<?php include $_SERVER['DOCUMENT_ROOT'] . '/../templates/footer.php'; ?>
